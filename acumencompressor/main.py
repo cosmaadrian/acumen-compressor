9f5983a1d27e61cc3f0c2ff02f8b551a5716ec1e
Adrian Cosma <cosma.i.adrian@gmail.com>
2024-06-06 17:30:12 +0300

    made it work with lists

--- acumencompressor/main.py
+++ acumencompressor/main.py
@@ -3,12 +3,18 @@
 import unicodedata
 import dahuffman
 import tqdm
+from collections import Counter, defaultdict
 
-def get_stats(ids, counts=None):
-    counts = {} if counts is None else counts
-    for pair in zip(ids, ids[1:]): # iterate consecutive elements
-        counts[pair] = counts.get(pair, 0) + 1
+def get_stats(ids, counts = None):
+    counts = dict(Counter(zip(ids, ids[1:])))
     return counts
+
+def dsum(dicts):
+    ret = defaultdict(int)
+    for d in dicts:
+        for k, v in d.items():
+            ret[k] += v
+    return dict(ret)
 
 def merge(ids, pair, idx):
     newids = []
@@ -20,44 +26,51 @@ def merge(ids, pair, idx):
         else:
             newids.append(ids[i])
             i += 1
+
     return newids
-
-def replace_control_characters(s: str) -> str:
-    chars = []
-    for ch in s:
-        if unicodedata.category(ch)[0] != "C":
-            chars.append(ch) # this character is ok
-        else:
-            chars.append(f"\\u{ord(ch):04x}") # escape
-    return "".join(chars)
-
-def render_token(t: bytes) -> str:
-    s = t.decode('utf-8', errors='replace')
-    s = replace_control_characters(s)
-    return s
 
 class BPETokenizer:
     """Base class for Tokenizers"""
 
     def __init__(self):
-        # default: vocab size of 256 (all bytes), no merges, no patterns
         self.merges = {} # (int, int) -> int
-        self.pattern = "" # str
-        self.special_tokens = {} # str -> int, e.g. {'<|endoftext|>': 100257}
-        self.vocab = self._build_vocab() # int -> bytes
 
-    def _build_vocab(self):
-        # vocab is simply and deterministically derived from merges
-        vocab = {idx: bytes([idx]) for idx in range(256)}
-        for (p0, p1), idx in self.merges.items():
-            vocab[idx] = vocab[p0] + vocab[p1]
-        for special, idx in self.special_tokens.items():
-            vocab[idx] = special.encode("utf-8")
-        return vocab
+    def train_on_lists(self, list_of_texts, vocab_size, verbose):
+        vocab_size = max(vocab_size, 256) # cannot have vocab size smaller than number of 8-bit bytes
+        num_merges = vocab_size - 256
+
+        merges = {} # (int, int) -> int
+        vocab = {idx: bytes([idx]) for idx in range(256)} # int -> bytes
+        
+        if verbose:
+            range_fn = tqdm.trange
+        else:
+            range_fn = range
+
+        # secret sauce?
+        ids = sorted([list(text_bytes) for text_bytes in list_of_texts])
+
+        for i in range_fn(num_merges):
+            stats = dsum([
+                get_stats(sub_ids)
+                for sub_ids in ids
+            ])
+            
+            pair = max(stats, key=stats.get)
+            idx = 256 + i
+            
+            ids = [
+                merge(sub_ids, pair, idx)
+                for sub_ids in ids
+            ]
+            
+            merges[pair] = idx
+            vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
+
+        return merges, vocab
 
     def train(self, text, vocab_size, verbose = False):
         vocab_size = max(vocab_size, 256) # cannot have vocab size smaller than number of 8-bit bytes
-
         num_merges = vocab_size - 256
 
         text_bytes = text
@@ -73,15 +86,18 @@ class BPETokenizer:
 
         for i in range_fn(num_merges):
             stats = get_stats(ids)
+            
             pair = max(stats, key=stats.get)
             idx = 256 + i
+            
             ids = merge(ids, pair, idx)
+            
             merges[pair] = idx
             vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
 
         return merges, vocab
 
-    def decode(self, ids, merges, vocab):
+    def decode(self, ids, vocab):
         text_bytes = b"".join(vocab[idx] for idx in ids)
         return text_bytes
 
@@ -110,6 +126,39 @@ class AcumenCompressor():
         self.vocab_size = vocab_size
         self.verbose = verbose
 
+    def compress_list(self, list_of_targets):
+        merges, vocab = self.bpe.train_on_lists(list_of_targets, vocab_size = self.vocab_size, verbose = self.verbose)
+
+        ids = [
+            self.bpe.encode(t, merges, vocab)
+            for t in list_of_targets
+        ]
+
+        codec = self.huffman.from_data(sorted(sum(ids, [])))
+
+        self.codec = codec
+        self.merges = merges
+        self.vocab = vocab
+
+        compressed = [
+            codec.encode(sub_ids)
+            for sub_ids in ids
+        ]
+
+        return compressed
+
+    def uncompress_list(self, list_of_targets):
+        ids = [
+            self.codec.decode(compressed_ids)
+            for compressed_ids in list_of_targets
+        ]
+        uncompressed = [
+            self.bpe.decode(sub_ids, vocab = self.vocab)
+            for sub_ids in ids
+        ]
+
+        return uncompressed
+
     def compress(self, target): # should be utf-8 bytes, returns bytes
         merges, vocab = self.bpe.train(target, vocab_size = self.vocab_size, verbose = self.verbose)
         ids = self.bpe.encode(target, merges, vocab)
@@ -124,7 +173,7 @@ class AcumenCompressor():
 
     def uncompress(self, compressed_text):
         ids = self.codec.decode(compressed_text)
-        uncompressed = self.bpe.decode(ids, merges = self.merges, vocab = self.vocab)
+        uncompressed = self.bpe.decode(ids, vocab = self.vocab)
         return uncompressed
 
    
--- acumencompressor/test.py
+++ acumencompressor/test.py
@@ -1,21 +1,19 @@
 from main import AcumenCompressor
 import string
 import random
+import gzip
+import numpy as np
 
-test_corpus = '''
-    lololololololololo
-'''
 
-test_corpus = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(100000))
-
-if __name__ == '__main__':
-    ac = AcumenCompressor(vocab_size = 1024, verbose = True)
+def test_sanity():
+    test_corpus = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(1000))
+    ac = AcumenCompressor(vocab_size = 512, verbose = True)
 
     compressed = ac.compress(test_corpus.encode('utf-8'))
-    # print(compressed)
+    print(compressed)
 
     uncompressed = ac.uncompress(compressed).decode('utf-8', errors = 'replace')
-    # print(uncompressed)
+    print(uncompressed)
 
     assert test_corpus == uncompressed
     assert len(compressed) < len(test_corpus.encode('utf-8')), "No compression performed!!!"
@@ -25,3 +23,41 @@ if __name__ == '__main__':
 
     print("Compression ratio:", round((1 - len(compressed) / len(test_corpus.encode('utf-8'))) * 100, 2), '%')
     
+
+def test_ordering():
+    corpora = [
+        ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(1000))
+        for _ in range(100)
+    ]
+
+    sizes_gzip = []
+    sizes_ours = []
+    sizes_ours_orderless = []
+    for _ in range(10):
+        # 100 permutations
+        random.shuffle(corpora)
+        target_permutation = "".join(corpora)
+
+        # gzip
+        compressed_gzip = gzip.compress(target_permutation.encode('utf-8'))
+        sizes_gzip.append(len(compressed_gzip))
+
+        # # AcumenCompressor
+        ac = AcumenCompressor(vocab_size = 300, verbose = True)
+        compressed_ac = ac.compress(target_permutation.encode('utf-8'))
+        sizes_ours.append(len(compressed_ac))
+
+        # AcumenCompressor order agnostic
+        ac = AcumenCompressor(vocab_size = 300, verbose = True)
+        compressed_ac = ac.compress_list([c.encode('utf-8') for c in corpora])
+        sizes_ours_orderless.append(len(b"".join(compressed_ac)))
+
+        # print(ac.merges)
+
+    print('gzip Sizes:', sizes_gzip, np.mean(sizes_gzip), np.std(sizes_gzip))
+    print('AC Sizes:', sizes_ours, np.mean(sizes_ours), np.std(sizes_ours))
+    print('AC (orderless) Sizes:', sizes_ours_orderless, np.mean(sizes_ours_orderless), np.std(sizes_ours_orderless))
+
+if __name__ == '__main__':
+    test_sanity()
+    test_ordering()
